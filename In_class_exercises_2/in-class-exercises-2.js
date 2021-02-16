d8a89a360cc96fe5b9ad4a23647ee2253dcc195b
Christopher Williams <ChrisW@SccColorado.org>
2021-02-16 16:56:53 -0700

    Logic Correction 2

--- In_class_exercises_2/in-class-exercises-2.js
+++ In_class_exercises_2/in-class-exercises-2.js
@@ -1,9 +1,9 @@
-var array = [1,2,3,4,5,6,7,8,9,10];
+var array= [1,2,3,4,5,6,7,8,9,10];
 for (var Yoda=0; Yoda<10;Yoda++) {
     if (Yoda === 0) {
         console.log("even");
-        else if (Yoda & 2 === 0) {
-            console.log("even");
+    }else if (Yoda % 2 === 0) {
+            console.log("even")
         }
         else {
             console.log("odd");
