a1bcb8cb38dd692cbe5e383d1b41519bd240163b
Christopher Williams <ChrisW@SccColorado.org>
2021-02-16 01:21:45 -0700

    master

---
+++ In_class_exercises_2/in-class-exercises-2.js
@@ -0,0 +1,11 @@
+var array = [1,2,3,4,5,6,7,8,9,10];
+for (var Yoda=0; Yoda<10;Yoda++) {
+    if (Yoda === 0) {
+        console.log(Yoda + "even");
+        else if (Yoda & 2 === 0) {
+            console.log(Yoda + "even");
+        }
+        else {
+            console.log(Yoda + "odd");
+    }
+}
