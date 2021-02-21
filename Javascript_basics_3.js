d977b8114651f76c77bcbbcce64f762c7f9889a4
Christopher Williams <70450884+CWilliamsJusticeReskill@users.noreply.github.com>
2021-02-21 00:02:43 -0700

    corrected push

---
+++ Javascript_basics_3.js
@@ -0,0 +1,12 @@
+var yourName = {
+    name: "Christopher Williams",
+    age: '37',
+    status: 'Alive',
+    homeTown: 'Clearlake, CA',
+    currentLocation: 'Home'
+    }
+    console.log("Name: "+yourName.name,"Age: "+yourName.age,"Status: "+yourName.status,
+    "Home Town: "+yourName.homeTown,"Current Location: "+yourName.currentLocation);
+  
+ 
+  
